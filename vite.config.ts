5f04eac4d2026c2797a85d142363e096caa6c5ce
Mac <maconly2001@gmail.com>
2025-02-10 18:28:20 -0700

    Fix build setup and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // Ensure repoName is a valid string (avoid undefined or empty values)
-const repoName = process.env.GITHUB_REPOSITORY 
-  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` // Extracts the repo name from the full repo path (e.g., user/repo)
-  : '';
+const repoName = process.env.VITE_REPO_NAME || '';
 
 export default defineConfig({
   plugins: [react()],
-  base: repoName ? repoName : '/', // Use '/' if repoName is empty (for local development)
-});
+  base: repoName ? `/${repoName}/` : '/', // Use '/' if repoName is empty
+})
